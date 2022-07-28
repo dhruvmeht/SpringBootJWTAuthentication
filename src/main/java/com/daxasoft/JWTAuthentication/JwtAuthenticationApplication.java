3c629364f0ae9d9fb8425f628a03c252720af2d6
Dhruv Mehta <F5392195@FNB.CO.ZA>
2022-07-28 13:08:54 +0530

    commit from first

--- src/main/java/com/daxasoft/JWTAuthentication/JwtAuthenticationApplication.java
+++ src/main/java/com/daxasoft/JWTAuthentication/JwtAuthenticationApplication.java
@@ -9,7 +9,11 @@ public class JwtAuthenticationApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(JwtAuthenticationApplication.class, args); 
+
 		System.out.println("Printing the content of applicationContext"+run.toString()); //2 nd part
+
+		run.close();  //1st work
+
 		
 	}
 
